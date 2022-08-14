2b624f05738e255538ee4b9053a772b5eaeb992b
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-14 20:42:56 +0800

    Add styling to page and expense item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28th 2021</div>
-      <h2>Car insurance</h2>
-      <div>$200.98</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
     </div>
   );
 }
