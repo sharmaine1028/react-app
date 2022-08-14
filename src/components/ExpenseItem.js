51f7e7e85c2c0a7937f11b3fb467e9ad3c18ae41
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-14 20:23:22 +0800

    Add more content to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,11 @@
 function ExpenseItem() {
-  return <h2> Expense item! </h2>;
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <h2>Car insurance</h2>
+      <div>$200.98</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
