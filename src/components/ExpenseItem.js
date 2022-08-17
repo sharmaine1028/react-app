8125b9d7b260ce30b694ff03f04e9cb16fb6036f
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-17 16:36:44 +0800

    Add expense date component and edit expense item to include expense date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,19 @@
+import React from "react";
+
 import "./ExpenseItem.css";
+import Card from "./Card";
+
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocateString("en-US", { month: "long" });
-  const day = props.date.toLocateString("en-US", { dat: "2-digit" });
-  const year = props.date.getFullYear();
   return (
-    <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
