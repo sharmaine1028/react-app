4642915a59be01d1c24f4a5cfff4a6d9becb0258
sharmaine1028 <sharmaine1028@gmail.com>
2022-08-21 15:50:26 +0800

    Create new folder UI to keep UI elements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseItem.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
